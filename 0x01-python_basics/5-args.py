6969cba0ac6254f72ba893bba1fc4a0bf8fc7613
Adekunle Adeniran <adekunle.adeniran@affirm.com>
2019-10-25 15:13:35 -0700

    Argument program

--- 0x01-python_basics/5-args.py
+++ 0x01-python_basics/5-args.py
@@ -2,15 +2,16 @@
 """Program to print arguments"""
 from sys import argv
 
-res = argv[1:]
-if len(res) == 0:
-    print("0 arguments.")
-else:
-    if len(res) == 1:
-        print("{} argument:".format(len(res)))
+if __name__ == "__main__":
+    res = argv[1:]
+    if len(res) == 0:
+        print("0 arguments.")
     else:
-        print("{} arguments:".format(len(res)))
-    count = 1
-    for items in res:
-        print("{}: {}".format(count, items))
-        count += 1
+        if len(res) == 1:
+            print("{} argument:".format(len(res)))
+        else:
+            print("{} arguments:".format(len(res)))
+        count = 1
+        for items in res:
+            print("{}: {}".format(count, items))
+            count += 1
