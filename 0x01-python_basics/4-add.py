59d9f6afa37196a9f4e78408ab504f00cd69956c
Adekunle Adeniran <adekunle.adeniran@affirm.com>
2019-10-24 17:10:23 -0700

    Import adder function

--- 0x01-python_basics/4-add.py
+++ 0x01-python_basics/4-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Python Program to import and add numbers"""
 if __name__ == "__main__":
     from add_4 import add
-    a, b = 1, 2
+    a = 1
+    b = 2
+
     print("{} + {} = {}".format(a, b, add(a, b)))
