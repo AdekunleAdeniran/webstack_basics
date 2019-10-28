550b86ab1f376ba2049e84750c8bd00d10661edd
flourishcodes <adekunle.adeniran@affirm.com>
2019-10-27 17:19:10 -0700

    Function to find max in integer

---
+++ 0x01-python_basics/7-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python function to find max integer in list"""
+
+
+def max_integer(my_list=[]):
+    """Python function to find max integer in list"""
+    if len(my_list) == 0:
+        return my_list
+    else:
+        bigger = 0
+        for items in my_list:
+            if items >= bigger:
+                bigger = items
+    return bigger
