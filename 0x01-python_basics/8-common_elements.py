8f112a946d26610fc3fa4acc806a45d8ae4315a2
flourishcodes <adekunle.adeniran@affirm.com>
2019-10-27 17:48:03 -0700

    Function to find commone elements in lists

---
+++ 0x01-python_basics/8-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Python function to find similar items in list"""
+
+
+def common_elements(set_1, set_2):
+    """Python function to find similar items in list"""
+    return list(set_1 & set_2)
