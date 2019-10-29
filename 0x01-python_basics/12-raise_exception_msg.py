38a254c773443114d51913e8268aee2815ca302f
flourishcodes <adekunle.adeniran@affirm.com>
2019-10-28 21:14:25 -0700

    Function that raises a name exception with a message..

---
+++ 0x01-python_basics/12-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Python function that raises a name exception with a message."""
+
+
+def raise_exception_msg(message=""):
+    """Python function that raises a name exception with a message."""
+    raise NameError(message)
