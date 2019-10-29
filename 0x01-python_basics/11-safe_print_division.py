d9df78982ec6e2df14ad5e575163380b2a8d64d7
flourishcodes <adekunle.adeniran@affirm.com>
2019-10-28 21:04:44 -0700

    Function that divides 2 integers and prints the result.

--- 0x01-python_basics/11-safe_print_division.py
+++ 0x01-python_basics/11-safe_print_division.py
@@ -7,7 +7,7 @@ def safe_print_division(a, b):
     try:
         division = a / b
     except ZeroDivisionError:
-        return None
+        division = None
     finally:
         print("Inside resut: {}".format(division))
     return division
